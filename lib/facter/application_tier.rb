2c6d53e56e66897df29cbf7724448a72c53360e3
Nick Walker <nick.walker@puppetlabs.com>
2015-10-13 12:18:44 -0700

    Add descriptive comments to application_tier fact

--- lib/facter/application_tier.rb
+++ lib/facter/application_tier.rb
@@ -1,6 +1,8 @@
 require 'facter'
 Facter.add('application_tier') do
 setcode do
-    Facter.value(:hostname).split('-')[1]
+     #returns the second position in a hostname
+     #An example, blah-test-red would return test
+     Facter.value(:hostname).split('-')[1]
    end
 end
