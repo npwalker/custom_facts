66e912f46da8001bb97851670a56ba1f4403df5b
Nick Walker <nick.walker@puppetlabs.com>
2015-11-16 09:35:44 -0800

    Fix whitespace on application_tier fact

--- lib/facter/application_tier.rb
+++ lib/facter/application_tier.rb
@@ -1,6 +1,7 @@
 require 'facter'
+
 Facter.add('application_tier') do
-setcode do
+  setcode do
      #returns the second position in a hostname
      #An example, blah-test-red would return test
      Facter.value(:hostname).split('-')[1]
